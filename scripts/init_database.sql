b5f5a246930e91b5409fd1f9f1f69454261fc8c5
Arthur Nweke-Uchebo <149632962+arthuruchebo@users.noreply.github.com>
2025-05-17 02:05:14 +0100

    Update and rename placeholder to init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,13 @@
+/*CREATE DATABASE DataWarehouse;
+/*
+===================================================
+Create Database
+===================================================
+Script Purpose:
+	This script creates a new database, 'dataWarehouse', after checking if it  already exists.
+    If the database exists, an error message will come up.
+*/
+
+
+Use DataWarehouse;
+
